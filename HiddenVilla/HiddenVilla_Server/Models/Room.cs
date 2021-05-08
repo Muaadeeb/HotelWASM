770c274725202ef7400ac5069c81d28bb323e55f
muaadeeb <timothyingledue@gmail.com>
2021-05-08 11:06:45 -0500

    #0000 - Day 1 - Initial Commit

---
+++ HiddenVilla/HiddenVilla_Server/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<RoomProperties> RoomProperties { get; set; }
+
+    }
+}
